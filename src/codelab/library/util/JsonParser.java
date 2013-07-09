32c685a6a2bbcdef86b4e7c5ddb065e157a93cf4
Hovan Yoo <hovan@hovans.com>
2013-07-10 02:03:24 +0900

    Lock 화면 개선 - 키잠금 등..

--- src/codelab/library/util/JsonParser.java
+++ src/codelab/library/util/JsonParser.java
@@ -1,6 +1,7 @@
 package codelab.library.util;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,12 +18,15 @@ import codelab.library.log.LogByCodeLab;
 public class JsonParser {
 	public static<T extends Object> T getNewInstance(Class<T> type, String jsonString) throws Exception {
 		if(jsonString.length() == 0) return null;
-		
+
+		LogByCodeLab.d(type.getSimpleName() + " instance creating... ");
 		JSONObject result = new JSONObject(jsonString);
 		T object = type.newInstance();
 		Class<?> fieldType;
 		String fieldName;
-		
+
+		int arrayTypeIndex = 0;
+
 		for(Field f : type.getFields()) {
 			fieldType = f.getType();
 			fieldName = f.getName();
@@ -30,9 +34,10 @@ public class JsonParser {
 				if(result.has(fieldName)) {
 					if(int.class.equals(fieldType)) {
 						f.set(object, result.get(fieldName));
-					} else if(List.class.equals(fieldType)) {
-						Type gType = f.getGenericType();
-						f.set(object, getNewList(gType.getClass(), result.get(fieldName).toString()));
+					} else if(ArrayList.class.equals(fieldType)) {
+//						Type gType = ((ParameterizedType)f.getGenericType()).getActualTypeArguments()[0];
+//						LogByCodeLab.d(gType.toString() + " list creating... ");
+//						f.set(object, getNewList(arrayTypes[arrayTypeIndex++], result.get(fieldName).toString()));
 					} else if(String.class.equals(fieldType) == false && result.get(fieldName) instanceof JSONObject){
 						f.set(object, getNewInstance(fieldType, result.get(fieldName).toString()));
 					} else {
@@ -43,10 +48,10 @@ public class JsonParser {
 					}
 				}
 			} catch(Exception e) {
-				LogByCodeLab.w(f.getType() + " instance failed. " + result.get(f.getName()).toString());
+				LogByCodeLab.e(e, f.getType() + " instance failed. " + result.get(f.getName()).toString());
 			}
 		}
-		LogByCodeLab.v(type.getSimpleName() + " instance created. " + object.toString());
+		LogByCodeLab.d(type.getSimpleName() + " instance created. " + object.toString());
 		
 		return object;
 	}
@@ -55,7 +60,7 @@ public class JsonParser {
 		if(jsonString != null) {
 			JSONArray jsonArray = new JSONArray(jsonString);
 			ArrayList<T> list = new ArrayList<T>(jsonArray.length());
-			LogByCodeLab.v(type.getSimpleName() + " list created. size is " + list.size());
+			LogByCodeLab.d(type.getSimpleName() + " list created. size is " + list.size());
 			for(int i=0 ; i<jsonArray.length() ; ++i) {
 				JSONObject jsonObject = jsonArray.getJSONObject(i);
 				T item = getNewInstance(type, jsonObject.toString());
