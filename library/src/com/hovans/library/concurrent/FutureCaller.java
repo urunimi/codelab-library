ab500ff93c95c76ee79971d4158c395b24df89b7
Hovan Yoo <hovan@hovans.com>
2014-03-26 00:24:22 +0900

    - hovans.com 도메인 룰에 따라 패키지 변경

---
+++ library/src/com/hovans/library/concurrent/FutureCaller.java
@@ -0,0 +1,104 @@
+package com.hovans.library.concurrent;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * {@link ThreadGuest} 에서 수행한 루틴을 동기적으로 얻어오는 과정을 가이드한다.<br>
+ * <br>
+ * 내부적으로 {@link ThreadGuest}를 만들어 수행하고 결과를 위해 대기한다.
+ * 데드락이 발생하지 않도록 주의.
+ * 다른 {@link FutureCaller} 또는 {@link ThreadGuest} 안에서 실행시키지 말 것.<br>
+ * <br>
+ * 사용 예:<pre>
+ * Integer val = new FutureExecuter&lt;Integer&gt;() {
+ *     public Integer call() throws Exception {
+ *         // 이 위치의 작업은 ThreadHost에서 수행된다.
+ *         return 10;
+ *     }
+ * }
+ * .execute();    // call() 의 수행을 요청한 후 현재 스레드는 blocking 대기한다.
+ * // 수행이 끝나면 val 에 10 이 할당된다.
+ * </pre>
+ * @author Arngard
+ *
+ * @param <T> 실행 결과의 클래스
+ */
+public abstract class FutureCaller<T> implements Callable<T> {
+
+	private FutureTask<T> mFuture;
+
+	/**
+	 * {@link ThreadHost}의 스레드에서 작업을 수행하고,
+	 * 그 결과를 동기적으로 얻어오기 위한 정의를 생성한다.
+	 */
+	public FutureCaller() {
+		mFuture = new FutureTask<T>(this);
+	}
+
+	private final void executeImple() {
+		new ThreadGuest() {
+
+			@Override
+			public Object run(long waitTimeMillis) {
+				mFuture.run();
+				return null;
+			}
+		}
+		.execute();
+	}
+
+	/**
+	 * {@link #call()}에 대한 수행을 하고 그 결과를 리턴한다.
+	 * @return 결과. 문제가 발생하면 null.
+	 * @see #execute(long, TimeUnit)
+	 */
+	public T execute() {
+		try {
+			executeImple();
+			return mFuture.get();
+		} catch (CancellationException e) {
+			return null;
+		} catch (InterruptedException e) {
+			return null;
+		} catch (ExecutionException e) {
+			return null;
+		}
+	}
+
+	/**
+	 * {@link #call()}에 대한 수행을 하고 그 결과를 리턴한다.
+	 * @param timeout
+	 * @param unit
+	 * @return 결과. 문제가 발생하면 null.
+	 * @see #execute()
+	 */
+	public T execute(long timeout, TimeUnit unit){
+		try {
+			executeImple();
+			return mFuture.get(timeout, unit);
+		} catch (CancellationException e) {
+			return null;
+		} catch (InterruptedException e) {
+			return null;
+		} catch (ExecutionException e) {
+			return null;
+		} catch (TimeoutException e) {
+			return null;
+		}
+	}
+
+	/**
+	 * execute 를 했을 때 {@link ThreadHost}의 스레드에서 호출될 코드를 정의한다.
+	 * {@link ThreadHost}를 통한 실행은 이 메소드를 직접 호출하는 것이 아님에 주의.
+	 * {@link #execute()} 를 사용하자.
+	 * @see java.util.concurrent.Callable#call()
+	 */
+	@Override
+	public abstract T call() throws Exception;
+
+}
---
+++ library/src/com/hovans/library/widget/TimePreference.java
@@ -0,0 +1,91 @@
+package com.hovans.library.widget;
+
+import java.util.Calendar;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.DialogPreference;
+import android.text.format.DateFormat;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TimePicker;
+
+import com.hovans.library.R;
+
+/**
+ * Custom preference for time selection. Hour and minute are persistent and
+ * stored separately as ints in the underlying shared preferences under keys
+ * KEY.hour and KEY.minute, where KEY is the preference's key.
+ */
+public class TimePreference extends DialogPreference {
+
+	/** The widget for picking a time */
+	private TimePicker timePicker;
+
+	/** Default hour */
+	private static int DEFAULT_HOUR = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+
+	/** Default minute */
+	private static int DEFAULT_MINUTE = 0;
+
+	/**
+	 * Creates a preference for choosing a time based on its XML declaration.
+	 * 
+	 * @param context
+	 * @param attributes
+	 */
+	public TimePreference(Context context, AttributeSet attributes) {
+		super(context, attributes);
+	}
+
+	/**
+	 * Initialize time picker to currently stored time preferences.
+	 * 
+	 * @param view
+	 *            The dialog preference's host view
+	 */
+	@Override
+	public void onBindDialogView(View view) {
+		super.onBindDialogView(view);
+		timePicker = (TimePicker) view.findViewById(R.id.prefTimePicker);
+		int time = getSharedPreferences().getInt(getKey(), 0);
+		if(time > 100) {
+			try {
+				DEFAULT_HOUR = time/100;
+				DEFAULT_MINUTE = time%100;
+			} catch(Exception e) {
+				//Do nothing.
+			}
+		}
+		timePicker.setCurrentHour(DEFAULT_HOUR);
+		timePicker.setCurrentMinute(DEFAULT_MINUTE);
+		timePicker.setIs24HourView(DateFormat.is24HourFormat(timePicker.getContext()));
+	}
+
+	/**
+	 * Handles closing of dialog. If user intended to save the settings,
+	 * selected hour and minute are stored in the preferences with keys KEY.hour
+	 * and KEY.minute, where KEY is the preference's KEY.
+	 * 
+	 * @param okToSave
+	 *            True if user wanted to save settings, false otherwise
+	 */
+	@Override
+	protected void onDialogClosed(boolean okToSave) {
+		super.onDialogClosed(okToSave);
+		if (okToSave) {
+			timePicker.clearFocus();
+			SharedPreferences.Editor editor = getEditor();
+			int setValue = timePicker.getCurrentHour()*100 + timePicker.getCurrentMinute();
+			editor.putInt(getKey(), setValue);
+			editor.commit();
+			setSummary(getTimeString(setValue));
+		}
+	}
+	
+	public static String getTimeString(int time) {
+		Calendar cal = Calendar.getInstance();
+		cal.set(2012, 12, 17, time/100, time%100, 0);
+		return java.text.DateFormat.getTimeInstance().format(cal.getTime());
+	}
+}
