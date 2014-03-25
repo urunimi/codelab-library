8e22c0073c87bded1b7ca61ae1a4d3e5c088f65d
Hovan Yoo <hovan@hovans.com>
2014-03-25 20:53:15 +0900

    - Codelab Library package 변경 - Proguard Rules 변경

---
+++ library/src/com/codelab/library/concurrent/RunnableGuest.java
@@ -0,0 +1,22 @@
+package com.codelab.library.concurrent;
+
+/**
+ * 우선순위 큐의 동작을 위한 Runnable 래핑.
+ * 
+ * @author Arngard
+ *
+ */
+abstract class RunnableGuest implements Runnable, Comparable<RunnableGuest> {
+	
+	ThreadGuest mGuest;
+
+	public RunnableGuest(ThreadGuest guest) {
+		mGuest = guest;
+	}
+	
+	@Override
+	public int compareTo(RunnableGuest another) {
+		return mGuest.compareTo(another.mGuest);
+	}
+
+}
